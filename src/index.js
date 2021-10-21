0ee7afcfb5e9b0bfa3ba36089c34af6004895f31
qkrthdus605 <qkrthdus605@gmail.com>
2021-10-21 22:00:00 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // 프로젝트에 라우터 적용
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
