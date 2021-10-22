5555ef6a34b5ffa51a8d9dbd6814d9286353bd9a
qkrthdus605 <qkrthdus605@gmail.com>
2021-10-22 22:30:03 +0900

    실습 : URL 쿼리 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 const App = () => {
   return(
@@ -13,10 +14,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
+      <Route path={['/about', '/info']} component={About} />
+      <Route path="/profile/:usename" component={Profile} />
     </div>
   );
 };
