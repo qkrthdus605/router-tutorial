fca3c263c5e6bd92b6d6ece2206e9ecd670e3147
qkrthdus605 <qkrthdus605@gmail.com>
2021-10-22 22:12:11 +0900

    실습: Link 사용

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Home from './Home';
 const App = () => {
   return(
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
